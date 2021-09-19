c12a5410ccfa23c93c04cf0f4e0d5cb310546790
ogienkoura <pumpkteam3@gmail.com>
2021-09-19 17:29:40 +0300

    string "edited" not appears after editing comment

---
+++ l10-chat/lib/comments.php
@@ -0,0 +1,61 @@
+<?php
+
+const MESSAGES_STORAGE = __DIR__ . '/../storage/';
+
+
+function getDates(): array
+{
+    $items = scandir(MESSAGES_STORAGE);
+    $dates = preg_filter('/^\d{4}\-\d{2}\-\d{2}/', '$0', $items);
+    rsort($dates);
+    return $dates;
+}
+
+function getComments(string $date): array
+{
+    $dir = MESSAGES_STORAGE . $date;
+    $items = scandir($dir);
+    $files = preg_filter('/^\d{10}_[0-9a-f]{32}\.log$/', '$0', $items);
+    rsort($files);
+
+    $messages = [];
+    foreach ($files as $file) {
+        $messages[] = getComment($date, $file);
+    }
+    return $messages;
+}
+
+function getComment(string $date, string $file): array
+{
+    $rout = MESSAGES_STORAGE . "{$date}/{$file}";
+    if (!file_exists($rout)) {
+        exit('Comment not exists');
+    }
+    $comment = file_get_contents($rout);
+    $comment = unserialize($comment);
+    $comment['file'] = $file;
+
+    return $comment;
+}
+
+function getMessageString(string $name, string $comment, int $createdAt, ?int $updatedAt = null): string
+{
+    $data = [
+        'name' => $name,
+        'comment' => $comment,
+        'created_at' => $createdAt,
+        'updated_at' => $updatedAt,
+    ];
+
+    return serialize($data);
+}
+
+function refreshMessage(string $date, string $file, string $comment): void
+{
+    $rout = MESSAGES_STORAGE . "{$date}/{$file}";
+    if (!file_exists($rout)) {
+        exit('Comment not exists');
+    }
+
+    file_put_contents($rout, $comment);
+}
---
+++ l10-chat/save-message.php
@@ -0,0 +1,22 @@
+<?php
+
+$date = $_POST['date'] ?? null;
+$file = $_POST['file'] ?? null;
+
+if (empty($date) || empty($file)) {
+    exit('Got no file for edit');
+}
+
+require_once  __DIR__ . '/lib/comments.php';
+require_once __DIR__ . '/lib/response.php';
+
+
+$oldMessage = getComment($date, $file);
+
+
+$message = getMessageString($_POST['username'], $_POST['comment'], $oldMessage['created_at']);
+refreshMessage($date, $file, $message);
+
+redirect("/l10-chat/index.php?date={$date}");
+
+
